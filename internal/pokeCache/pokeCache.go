4f9f9609449bcdb20b75372fbf7d0d448403767a
HenningRixen <henning.rixen@freenet.de>
2024-11-20 21:59:34 +0100

    pokecache

--- internal/pokeCache/pokeCache.go
+++ internal/pokeCache/pokeCache.go
@@ -16,10 +16,12 @@ type cacheEntry struct {
 }
 
 func NewCache(interval time.Duration) Cache {
-	return Cache{
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 		mu:    &sync.Mutex{},
 	}
+	go c.ReapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, value []byte) {
@@ -33,4 +35,21 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	defer c.mu.Unlock()
 	foundCacheEntryValue, ok := c.cache[key]
 	return foundCacheEntryValue.val, ok
+}
+
+func (c *Cache) ReapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.Reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) Reap(now time.Time, last time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
 }
